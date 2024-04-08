c1a94e9d41deddaf9f8d86fb0cf9a27d8ca1270a
John <king.johnf@gmail.com>
2024-04-08 10:43:06 -0400

    feat: adds Terraform Install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-touch ~/dotfiles-was-here.txt
+# Install Terraform
+wget --quiet https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip \
+  && unzip terraform_1.7.5_linux_amd64.zip \
+  && mv terraform /usr/bin \
+  && rm terraform_1.7.5_linux_amd64.zip
