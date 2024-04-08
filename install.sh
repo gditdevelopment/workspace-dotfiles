db1394c19cb405b4a637025d46287af928293068
John <king.johnf@gmail.com>
2024-04-08 10:53:14 -0400

    feat: adds AWS CLI v2

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 cd $HOME
 
-# Install Terraform
+# Install Terraform 1.7.5
 wget --quiet https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip \
   && unzip terraform_1.7.5_linux_amd64.zip \
   && sudo mv terraform /usr/bin \
   && rm terraform_1.7.5_linux_amd64.zip
+
+# Install AWS CLIv2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+  && unzip awscliv2.zip \
+  && ./aws/install
