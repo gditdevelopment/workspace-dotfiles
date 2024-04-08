bfd64dae51f358d52c2b86448de17eaf0ac5b0c1
John <king.johnf@gmail.com>
2024-04-08 10:50:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cd $HOME
 # Install Terraform
 wget --quiet https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip \
   && unzip terraform_1.7.5_linux_amd64.zip \
-  && mv terraform /usr/bin \
+  && sudo mv terraform /usr/bin \
   && rm terraform_1.7.5_linux_amd64.zip
